b46176e7a7d3373a0cabb822e54906904e734745
Viktor Casubolo <viktor.casubolo98@gmail.com>
2021-05-12 17:24:53 +0200

    header terminato

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+        <link rel="stylesheet" href="/css/app.css">
+        <title>Laravel</title>
+    </head>
+    <body>
+        @include('partials.header')
+        <main>
+            @yield('main')
+            @yield('links')
+        </main>
+        @include('partials.footer')
+    </body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,30 @@
+<header>
+    <div class="info">
+        <div class="container">
+            <span>dc power visa</span>
+            <span class='options'>additional dc sites</span>
+        </div>
+    </div>
+    <nav class='container'>
+        <div class="logo">
+            <img src="images/dc-logo.png" alt="">
+        </div>
+        <ul>
+            <li><a href="/">characters</a></li>
+            <li><a class='active' href="/">comics</a></li>
+            <li><a href="/">movies</a></li>
+            <li><a href="/">tv</a></li>
+            <li><a href="/">games</a></li>
+            <li><a href="/">collectibles</a></li>
+            <li><a href="/">videos</a></li>
+            <li><a href="/">fans</a></li>
+            <li><a href="/">news</a></li>
+            <li><a href="/">shop</a></li>
+        </ul>
+        <input type="text" name="search" class="search" placeholder='Search'>
+    </nav>
+
+    <div class="jumptron">
+        
+    </div>
+</header>
