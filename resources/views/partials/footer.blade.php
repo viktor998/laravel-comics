3c725f354f96a917b2e5da85acba8eaa01cd8fea
Viktor Casubolo <viktor.casubolo98@gmail.com>
2021-05-13 17:38:13 +0200

    aggiornato footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,60 @@
 <footer>
+    <div class="bg_img">
+        <div class="logo">
+            <img src="/images/dc-logo-bg.png" alt="">
+        </div>
+        <div class="container">
+            <div class="links pt-10">
+                <div class="col-1">
+                    <h1>dc comics</h1>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Tv</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
 
+                    <h1>shop</h1>
+                    <ul>
+                        <li> <a href="#">Shop DC</a> </li>
+                        <li> <a href="#">Shop DC Collectibles </a> </li>
+                    </ul>
+                </div>
+                <div class="col-2">
+                    <h1>dc</h1>
+                    <ul>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="col-3">
+                    <h1>sites</h1>
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="rights pt-8 pb-10">
+                All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="">noted here</a>.
+                All rights reserved. <a href="#">Cookies Settings</a>
+            </div>
+        </div>
+    </div>
 </footer>
