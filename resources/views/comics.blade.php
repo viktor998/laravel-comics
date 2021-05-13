3cbd4cae5706f264f5175e4d64359b2bae552c8a
Viktor Casubolo <viktor.casubolo98@gmail.com>
2021-05-13 16:35:18 +0200

    main terminato

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -6,22 +6,68 @@
 
     <div class="bg_black">
         <div class="container">
-           <div class="comics">
-           @foreach($comics as $index => $comic)
-                <div class="comic">
-                    
-                        <a href="{{route('page', ['id' => $index])}}">
-                            <div href='/home' style="background-image: url({{ $comic['thumb']}});" class="comic-thumb"></div>
+            <div class="comics mb-12">
+                @foreach($comics as $index => $comic)
+                    <div class="comic">
                         
-                        </a>
-                    
-                    <div class="comic-title">
-                        <span > {{ ($comic['series']) }} </span>
+                            <a href="{{route('page', ['id' => $index])}}">
+                                <div href='/home' style="background-image: url({{ $comic['thumb']}});" class="comic-thumb"></div>
+                            
+                            </a>
+                        
+                        <div class="comic-title">
+                            <span > {{ ($comic['series']) }} </span>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+
+            <div class="button">
+                <input class='btn' type="button" value="LOAD MORE">
+            </div>
+        </div>
+
+        <div class="bg_blue_2">
+
+            <div class="container">
+                <div class="in">
+                    <div class="in-0">
+                        <div class="img-cut">
+                            <img src="/images/buy-comics-digital-comics.png" alt="">
+                        </div>
+                        <span>digital comics </span>
+                        
+                    </div>
+                    <div class="in-1">
+                        <div class="img-cut">
+                            <img src="/images/buy-comics-merchandise.png" alt="">
+                        </div>
+                        <span>shop dc</span>
+                        
+                    </div>
+                    <div class="in-2">
+                        <div class="img-cut">
+                            <img src="/images/buy-comics-subscriptions.png" alt="">
+                        </div>
+                        <span>comic shop locator</span>
+                        
+                    </div>
+                    <div class="in-3">
+                        <div class="img-cut">
+                            <img src="/images/buy-comics-shop-locator.png" alt="">
+                        </div>
+                        <span>subscriptions</span>
+                        
+                    </div>
+                    <div class="in-3">
+                        <div class="img-cut">
+                            <img src="/images/buy-dc-power-visa.svg" alt="">
+                        </div>
+                        <span>dc power visa</span>
+                        
                     </div>
                 </div>
-            @endforeach
-           </div>
-        </div>
+            </div>
     </div>
 
     
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -5,21 +5,23 @@
     <div class="bg_blue">
         <img src="{{$comic['thumb']}}" alt="">
     </div>
-    <div class="container comic_page">
+    <div class="comic_page">
 
         
+        <div class="container">
         <div class="content mt-16">
-            <div class="text mr-8">
+            <div class="text mr-24">
                 <h1 class='title'>
                      {{$comic['title']}} 
                 </h1> 
-                <div class="price_available">
-                    <p>U.S. Price:  {{$comic['price']}} </p>
-                    <p>AVAILABLE</p>
-                    <span>Check Availability</span>
+                <div class="price_available mt-8 mb-8">
+                    <p class='ml-6'>U.S. Price:  <strong>{{$comic['price']}}</strong> </p>
+                    <p class='avv'>AVAILABLE</p>
+                    <h4>Check Availability</h4>
+                    
                 </div>
                 <div class="description">
-                     {{$comic['description']}} 
+                    {{$comic['description']}} 
                 </div>
             </div>
             <div class="ads">
@@ -27,12 +29,102 @@
                 <img src="../images/adv.jpg" alt="">
             </div>
         </div>
+        </div>
+
+        <div class="talent-specs mt-16 pb-16">
+            <div class="container">
+                <div class="talent">
+                    <h1 class='mt-4 mb-4'>Talent</h1>
+                    <hr>
+                    <div class="art-by pt-2 pb-2">
+                        <span>Art by:</span>
+                        <div class="artists">
+                            @foreach($comic['artists'] as $artist)
+                                <h5>  {{$artist}},   </h5> 
+                            @endforeach
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="written-by pt-2 pb-2">
+                        <span>Written by:</span>
+                        <div class="artists">
+                            @foreach($comic['writers'] as $writer)
+                                <h5>  {{$writer}},   </h5> 
+                            @endforeach
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+                <div class="specs">
+                    <h1 class='mt-4 mb-4'>Specs</h1>
+                    <hr>
+                    <div class="series pt-2 pb-2">
+                        <span>Series:</span>
+                        <div class="serie">
+                            
+                                <p class='title'>  {{$comic['series']}}   </p> 
+                            
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="prices pt-2 pb-2">
+                        <span>U.S. Price:</span>
+                        <div class="price">
+                           
+                                <p>  {{$comic['price']}}   </p> 
+                            
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="sale-date pt-2 pb-2">
+                        <span>U.S. Price:</span>
+                        <div class="date">
+                           
+                                <p>  {{$comic['sale_date']}}   </p> 
+                            
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+
+               
+            </div>
+           
+        </div>
+
+         <div class="info-container">
+            <div class="container">
+                <div class="info">
+                    <div class="info-0">
+                        <span>digital comics </span>
+                        <div class="image-cut">
+                            <img src="/images/cta-icons.png" alt="">
+                        </div>
+                    </div>
+                    <div class="info-1">
+                        <span>shop dc</span>
+                        <div class="image-cut">
+                            <img src="/images/cta-icons.png" alt="">
+                        </div>
+                    </div>
+                    <div class="info-2">
+                        <span>comic shop locator</span>
+                        <div class="image-cut">
+                            <img src="/images/cta-icons.png" alt="">
+                        </div>
+                    </div>
+                    <div class="info-3">
+                        <span>subscriptions</span>
+                        <div class="image-cut">
+                            <img src="/images/cta-icons.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+         </div>
         
         
-            @foreach($comic['artists'] as $artist)
-                <span>  {{$artist}} </span> 
-                
-            @endforeach
+            
         
     </div>
 
